ac7d8f9d100978559c06f9deccbfe95e453296d3
semssk <ssksem015@gmail.com>
2023-01-27 14:09:10 +0100

    changed humidity use server

--- src/tsLibs/humidityModel.ts
+++ src/tsLibs/humidityModel.ts
@@ -1,6 +1,18 @@
-import { getRandomInRange } from "./core"
+import axios from "axios";
+import { navigate } from "svelte-routing";
+import { axiosConfig, getRandomInRange, getServerUrl } from "./core"
 
 
 const getHumidity = () => Math.floor(getRandomInRange(40,50));
 
-export const affectHumidity = (affect:(val:string)=>void) => affect(getHumidity().toString() + "g/m3");
+export const affectHumidity = (affect:(val:string)=>void) => {
+    axios.get(getServerUrl() + "humidity",axiosConfig())
+        .then((res)=>{
+            if(res.status === 403){
+                navigate("/");
+            }
+            else{
+                affect(parseInt(res.data).toString() + " g/m3")
+            }
+        })
+};
